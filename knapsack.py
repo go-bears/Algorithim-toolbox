1abb060e14a7bdcd825af61a1a78f73bcf4ef373
go-bears <melissa.fabros@gmail.com>
2016-04-25 23:12:19 -0700

    added more use cases

--- knapsack.py
+++ knapsack.py
@@ -55,4 +55,34 @@ if __name__ == '__main__':
     print 'maximized knapsack val', knapsack()
     print timeit.timeit(lambda: knapsack(), number=100)
 
+def knapsack2(dct, capacity):
     
+    items = dct
+    fractional = []
+
+    knapsack = []
+    knapsack_val = 0
+    knapsack_capacity = capacity
+
+    # build array of fractional unit cost 
+    for item, value in items.iteritems():
+        for i in range(int(value[1])):
+            unit_cost = value[0]/value[1]
+            fractional.append(unit_cost)
+
+    # sort the fractional list of items from high to low
+    fractional.sort(reverse=True)
+
+    # building the knapsack
+    for unit in fractional:
+        if len(knapsack) < knapsack_capacity: 
+            knapsack.append(unit)
+            knapsack_val += unit
+
+    return format(knapsack_val, '.4f')
+
+print knapsack2({1:[60,20.00], 2:[100,50.00], 3:[120, 30.00]}, 50)
+
+print knapsack2({1:[500,30.00]}, 10)
+
+
